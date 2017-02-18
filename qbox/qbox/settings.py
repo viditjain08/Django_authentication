db31915907b9374aedb008ae8a2bc96666f401fa
viditjain08 <viditjain08@gmail.com>
2017-02-19 01:00:04 +0530

    Login logout done

--- qbox/qbox/settings.py
+++ qbox/qbox/settings.py
@@ -55,7 +55,7 @@ AUTH_USER_MODEL = 'main.Erpuser'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
